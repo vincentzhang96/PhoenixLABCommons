8f3624e99c55df69d511f6b7f837176a27b45bdf
Vincent Zhang <vzhang@purdue.edu>
2015-07-23 11:44:37 -0400

    Added helper class for parsing digits

---
+++ src/main/java/co/phoenixlab/common/number/Digit.java
@@ -0,0 +1,41 @@
+package co.phoenixlab.common.number;
+
+/**
+ * Internal helper class for parsing decimal and hexadecimal digits
+ */
+class Digit {
+
+    private Digit() {}
+
+    /**
+     * Returns the integer value of a character or throws an exception if the character does not represent a decimal
+     * digit. Decimal digits are characters matching {@code [0-9]}.
+     * @param c The character to convert to an integer value.
+     * @return The integer value of the character, values from 0 to 9 inclusive.
+     * @throws NumberFormatException If the provided character falls outside of {@code [0-9]}
+     */
+    static int decDigit(char c) throws NumberFormatException {
+        if (c >= '0' && c <= '9') {
+            return c - '0';
+        }
+        throw new NumberFormatException("Unknown dec digit character '" + c + "'");
+    }
+
+    /**
+     * Returns the integer value of a character or throws an exception if the character does not represent a hexadecimal
+     * digit. Hexadecimal digits are characters matching {@code [0-9A-Z]}. This method does NOT accept lowercase.
+     * @param c The character to convert to an integer value.
+     * @return The integer value of the character, values from 0 to 15 inclusive.
+     * @throws NumberFormatException If the provided character falls outside of {@code [0-9A-Z]}
+     */
+    static int hexDigit(char c) throws NumberFormatException {
+        if (c >= '0' && c <= '9') {
+            return c - '0';
+        }
+        if (c >= 'A' && c <= 'F') {
+            //  equiv to c - 'A' + 10, since 'A' is ASCII #65 and '7' is #55
+            return c - '7';
+        }
+        throw new NumberFormatException("Unknown hex digit character '" + c + "'");
+    }
+}
