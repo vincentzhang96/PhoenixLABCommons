17522d3ecd1fd5fbe72c387eab826421f1d33b6f
Vincent Zhang <vzhang@purdue.edu>
2015-08-05 10:47:22 -0400

    Add active locale to provider

--- src/main/java/co/phoenixlab/common/localization/LocaleStringProvider.java
+++ src/main/java/co/phoenixlab/common/localization/LocaleStringProvider.java
@@ -1,6 +1,10 @@
 package co.phoenixlab.common.localization;
 
+import java.util.Locale;
+
 public interface LocaleStringProvider {
+
+    void setActiveLocale(Locale locale);
 
     String get(String key);
 
