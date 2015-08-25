5ffcf1d64375b51104daf433be81a1f68d27e182
Vincent Zhang <vzhang@purdue.edu>
2015-08-24 20:18:25 -0400

    Add integer type tests

--- src/test/java/co/phoenixlab/common/lang/io/LittleEndianDataInputStreamTest.java
+++ src/test/java/co/phoenixlab/common/lang/io/LittleEndianDataInputStreamTest.java
@@ -12,7 +12,7 @@ public class LittleEndianDataInputStreamTest {
     @Test
     public void testReadByte() throws Exception {
         byte b = 0x12;
-        ByteArrayInputStream bais = new ByteArrayInputStream(new byte[] {b});
+        ByteArrayInputStream bais = new ByteArrayInputStream(new byte[]{b});
         DataInputStream dis = new DataInputStream(bais);
         LittleEndianDataInputStream ledis = new LittleEndianDataInputStream(dis);
         assertEquals(b, ledis.readByte());
@@ -21,7 +21,7 @@ public class LittleEndianDataInputStreamTest {
     @Test
     public void testReadUnsignedByte() throws Exception {
         int i = 200;
-        ByteArrayInputStream bais = new ByteArrayInputStream(new byte[] {(byte) i});
+        ByteArrayInputStream bais = new ByteArrayInputStream(new byte[]{(byte) i});
         DataInputStream dis = new DataInputStream(bais);
         LittleEndianDataInputStream ledis = new LittleEndianDataInputStream(dis);
         assertEquals(i, ledis.readUnsignedByte());
@@ -29,32 +29,65 @@ public class LittleEndianDataInputStreamTest {
 
     @Test
     public void testReadShort() throws Exception {
-
+        short i = 500;
+        ByteArrayInputStream bais = new ByteArrayInputStream(new byte[]{(byte) (i & 0xFF), (byte) ((i >> 8) & 0xFF)});
+        DataInputStream dis = new DataInputStream(bais);
+        LittleEndianDataInputStream ledis = new LittleEndianDataInputStream(dis);
+        assertEquals(i, ledis.readShort());
     }
 
     @Test
     public void testReadUnsignedShort() throws Exception {
-
+        int i = 0xFF20;
+        short c = (short) i;
+        ByteArrayInputStream bais = new ByteArrayInputStream(new byte[]{(byte) (c & 0xFF), (byte) ((c >> 8) & 0xFF)});
+        DataInputStream dis = new DataInputStream(bais);
+        LittleEndianDataInputStream ledis = new LittleEndianDataInputStream(dis);
+        assertEquals(i, ledis.readUnsignedShort());
     }
 
     @Test
     public void testReadChar() throws Exception {
-
+        char c = 0xFF20;
+        ByteArrayInputStream bais = new ByteArrayInputStream(new byte[]{(byte) (c & 0xFF), (byte) ((c >> 8) & 0xFF)});
+        DataInputStream dis = new DataInputStream(bais);
+        LittleEndianDataInputStream ledis = new LittleEndianDataInputStream(dis);
+        assertEquals(c, ledis.readChar());
     }
 
     @Test
     public void testReadInt() throws Exception {
-
+        int i = 0xDEADBEEF;
+        ByteArrayInputStream bais = new ByteArrayInputStream(new byte[]{(byte) (i & 0xFF), (byte) ((i >> 8) & 0xFF),
+                (byte) ((i >> 16) & 0xFF), (byte) ((i >> 24) & 0xFF)});
+        DataInputStream dis = new DataInputStream(bais);
+        LittleEndianDataInputStream ledis = new LittleEndianDataInputStream(dis);
+        assertEquals(i, ledis.readInt());
     }
 
     @Test
     public void testReadUnsignedInt() throws Exception {
-
+        int i = 0xFAFEBABE;
+        long l = Integer.toUnsignedLong(i);
+        ByteArrayInputStream bais = new ByteArrayInputStream(new byte[]{(byte) (i & 0xFF), (byte) ((i >> 8) & 0xFF),
+                (byte) ((i >> 16) & 0xFF), (byte) ((i >> 24) & 0xFF)});
+        DataInputStream dis = new DataInputStream(bais);
+        LittleEndianDataInputStream ledis = new LittleEndianDataInputStream(dis);
+        assertEquals(l, ledis.readUnsignedInt());
     }
 
     @Test
     public void testReadLong() throws Exception {
-
+        long l = 0xCAFEBABEDEADBEEFL;
+        ByteArrayInputStream bais = new ByteArrayInputStream(new byte[]{
+                (byte) (l & 0xFF), (byte) ((l >> 8) & 0xFF),
+                (byte) ((l >> 16) & 0xFF), (byte) ((l >> 24) & 0xFF),
+                (byte) ((l >> 32) & 0xFF), (byte) ((l >> 40) & 0xFF),
+                (byte) ((l >> 48) & 0xFF), (byte) ((l >> 56) & 0xFF)
+        });
+        DataInputStream dis = new DataInputStream(bais);
+        LittleEndianDataInputStream ledis = new LittleEndianDataInputStream(dis);
+        assertEquals(l, ledis.readLong());
     }
 
     @Test
