238575e574b91b176869ddfabfdc35a4d34ae28a
Vincent Zhang <vzhang@purdue.edu>
2015-12-06 17:12:01 -0500

    Add plurality tests

--- src/test/java/co/phoenixlab/common/localization/LocalizerTest.java
+++ src/test/java/co/phoenixlab/common/localization/LocalizerTest.java
@@ -17,6 +17,7 @@ public class LocalizerTest {
     public void setup() {
         strings = new HashMap<>();
         localizer = new LocalizerImpl(Locale.US);
+        localizer.registerPluralityRules(LocalizerImpl.defaultPluralityRules());
         localizer.addLocaleStringProvider(new LocaleStringProvider() {
             @Override
             public void setActiveLocale(Locale locale) {
@@ -57,5 +58,55 @@ public class LocalizerTest {
         final String ret = localizer.localize(key, arg0, arg1);
         assertEquals(expected, ret);
     }
-    
+
+    @Test
+    public void testSimplePluralOne() throws Exception {
+        final String key = "test.string";
+        final String val = "{0|%d} {0|(one;potato),(en_plural;potatoes)}";
+        final int arg0 = 1;
+        final String expected = "1 potato";
+        strings.clear();
+        strings.put(key, val);
+        final String ret = localizer.localize(key, arg0);
+        assertEquals(expected, ret);
+    }
+
+    @Test
+    public void testSimplePluralEnPlural() throws Exception {
+        final String key = "test.string";
+        final String val = "{0|%d} {0|(one;potato),(en_plural;potatoes)}";
+        final int arg0 = 2;
+        final String expected = "2 potatoes";
+        strings.clear();
+        strings.put(key, val);
+        final String ret = localizer.localize(key, arg0);
+        assertEquals(expected, ret);
+    }
+
+    @Test
+    public void testOrPluralEnPlural() throws Exception {
+        final String key = "test.string";
+        final String val = "{0|(one,en_plural;potatoes)}";
+        final int arg1 = 1;
+        final int arg2 = 2;
+        final String expected = "potatoes";
+        strings.clear();
+        strings.put(key, val);
+        final String ret1 = localizer.localize(key, arg1);
+        final String ret2 = localizer.localize(key, arg2);
+        assertEquals(expected, ret1);
+        assertEquals(expected, ret2);
+    }
+
+    @Test
+    public void testAndPluralEnPlural() throws Exception {
+        final String key = "test.string";
+        final String val = "{0|(zero+en_plural;potatoes),(en_plural;potato)}";
+        final int arg1 = 2;
+        final String expected = "potato";
+        strings.clear();
+        strings.put(key, val);
+        final String ret1 = localizer.localize(key, arg1);
+        assertEquals(expected, ret1);
+    }
 }
